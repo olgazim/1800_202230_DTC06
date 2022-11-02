5e2ca458539e84662cf263d5602345f77f54ab45
Olga Zimina <olga.vic.zimina@gmail.com>
2022-11-01 22:12:10 -0700

    Add firebase configuration

---
+++ scripts/firebase.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { auth, initializeApp } = window.firebase;
+
+//----------------------------------------
+//  Firebase configuration
+//----------------------------------------
+  const firebaseConfig = {
+    apiKey: "AIzaSyDDJ9Pa2j2d6HWpUyv87_1rGE3yugiOO48",
+    authDomain: "meditrack-1a12b.firebaseapp.com",
+    projectId: "meditrack-1a12b",
+    storageBucket: "meditrack-1a12b.appspot.com",
+    messagingSenderId: "118473868125",
+    appId: "1:118473868125:web:ce8177952102a88e029eb4"
+  };
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database 
+//--------------------------------------------
+initializeApp(firebaseConfig);
+const authClient = auth();
+window.authClient = authClient
