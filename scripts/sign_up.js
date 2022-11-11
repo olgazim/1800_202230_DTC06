51732b9e4babcf4c6e4ff6baa2d8fdce632396a9
jaason-shi <jasons8002@gmail.com>
2022-11-11 15:52:34 -0800

    implement users collection to firebase

--- scripts/sign_up.js
+++ scripts/sign_up.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
 /*
  * Get DOM nodes <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
  * */
 
-const errorMessageContainer = document.getElementById('error-message');
-const form = document.getElementById('sign-up-form');
-const inputPassword = document.getElementById('password');
-const inputPasswordConfirm = document.getElementById('password-confirm');
-const inputShowPassword = document.getElementById('show-password');
+const errorMessageContainer = document.getElementById("error-message");
+const form = document.getElementById("sign-up-form");
+const inputPassword = document.getElementById("password");
+const inputPasswordConfirm = document.getElementById("password-confirm");
+const inputShowPassword = document.getElementById("show-password");
 
 /*
  * Firebase actions <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
@@ -25,8 +25,8 @@ const signUp = async (email, password) => {
 const togglePasswordVisibility = (event) => {
   const checked = event.target.checked;
 
-  inputPassword.type = checked ? 'text' : 'password';
-  inputPasswordConfirm.type = checked ? 'text' : 'password';
+  inputPassword.type = checked ? "text" : "password";
+  inputPasswordConfirm.type = checked ? "text" : "password";
 };
 
 const onPasswordChange = () => {
@@ -34,10 +34,10 @@ const onPasswordChange = () => {
   const passwordConfirmValue = inputPasswordConfirm.value;
   const arePasswordsMatch = passwordValue === passwordConfirmValue;
 
-  if (passwordValue !== '' && arePasswordsMatch) {
-    inputPasswordConfirm.setCustomValidity('');
+  if (passwordValue !== "" && arePasswordsMatch) {
+    inputPasswordConfirm.setCustomValidity("");
   } else {
-    inputPasswordConfirm.setCustomValidity('Passwords do not match');
+    inputPasswordConfirm.setCustomValidity("Passwords do not match");
   }
 };
 
@@ -58,31 +58,44 @@ const onSubmit = async (event) => {
     // fetch Firebase auth API
     try {
       const authData = await signUp(email, password);
-
-      console.log('Auth data:', authData);
+      var users = authData.user;
+      console.log("Auth data:", authData);
       // Actions when user is created (e.g. redirect to dashboard)
-      window.alert('Congratulations!\n You\'ve successfully created account in MediTrack.');
-      window.location.href = "home.html";
+      db.collection("users")
+        .doc(users.uid)
+        .set({
+          name: form.elements[1].value,
+          email: form.elements[2].value,
+        })
+        .then(function () {
+          console.log("New user added to database");
+          window.alert(
+            "Congratulations!\n You've successfully created account in MediTrack."
+          );
+          window.location.href = "home.html";
+        });
     } catch (error) {
       // Actions when user creation failed (e.g. redirect to login page)
       // Check if user already exists
-      if (error.code === 'auth/email-already-in-use') {
+      if (error.code === "auth/email-already-in-use") {
         // Redirect to login page
-        window.alert('Account with this email is already a member of MediTrack. Use your email and password to sign in.');
-        window.location.href = "sign_in.html";
+        window.alert(
+          "Account with this email is already a member of MediTrack. Use your email and password to sign in."
+        );
+        window.location.href = "sign_in_scr.html";
       }
 
       // Show Firebase error message below the form
       errorMessageContainer.innerText = `Error from Firebase: ${error.message}`;
 
-      console.log('ERROR code:', error.code);
-      console.log('ERROR message:', error.message);
+      console.log("ERROR code:", error.code);
+      console.log("ERROR message:", error.message);
     }
   } else {
     // Actions when validation failed
-    form.classList.add('was-validated');
+    form.classList.add("was-validated");
 
-    console.log('Form is invalid');
+    console.log("Form is invalid");
   }
 };
 
@@ -91,11 +104,11 @@ const onSubmit = async (event) => {
  * */
 
 // Listen to form's onSubmit event to validate the form
-form.addEventListener('submit', onSubmit, false);
+form.addEventListener("submit", onSubmit, false);
 
 // Listen to "onKeyUp" event of the password inputs to validate the values for equality
-inputPassword.addEventListener('keyup', onPasswordChange, false);
-inputPasswordConfirm.addEventListener('keyup', onPasswordChange, false);
+inputPassword.addEventListener("keyup", onPasswordChange, false);
+inputPasswordConfirm.addEventListener("keyup", onPasswordChange, false);
 
 // Listen to "onChange" event of the checkbox input to show/hide password
-inputShowPassword.addEventListener('change', togglePasswordVisibility, false);
+inputShowPassword.addEventListener("change", togglePasswordVisibility, false);
