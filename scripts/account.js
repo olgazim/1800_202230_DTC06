67e28a1386cad0360893d0044097f7cb1b8f281e
Olga Zimina <olga.vic.zimina@gmail.com>
2022-11-18 22:23:11 -0800

    Refactor profile edit

--- scripts/account.js
+++ scripts/account.js
@@ -1,11 +1,22 @@
+
 'use strict';
 
-var currentUser
+var currentUser;
 
 //----------------------------------------
 //  Get DOM nodes
 //----------------------------------------
 const buttonSignOut = document.getElementById('sign-out');
+const buttonSave = document.getElementById('saveBtn');
+const buttonEdit = document.getElementById('editBtn');
+const avatarContainer = document.getElementById('avatar-image');
+const inputImage = document.getElementById('avatar_upload');
+const inputName = document.getElementById('userName');
+const inputEmail = document.getElementById('email');
+const inputProfile = document.getElementById('profileName');
+const listProfile = document.getElementById('profile-list');
+const fieldSetUserInfo = document.getElementById('userInfo');
+const fieldSetUserProfiles = document.getElementById('user-profiles');
 
 // Firebase actions
 
@@ -14,87 +25,160 @@ const signOut = () => {
 };
 
 
+
+//----------------------------------------
+//  Helpers
+//----------------------------------------
+
+
+const enterEditMode = () => {
+  fieldSetUserInfo.disabled = false;
+  fieldSetUserProfiles.disabled = false;
+
+  buttonSave.removeAttribute('hidden');
+  buttonEdit.setAttribute('hidden', '');
+};
+
+const exitEditMode = () => {
+  fieldSetUserInfo.disabled = true;
+  fieldSetUserProfiles.disabled = true;
+
+  buttonSave.setAttribute('hidden', '');
+  buttonEdit.removeAttribute('hidden');
+};
+
+
+
 //----------------------------------------
 //  Event listeners
 //----------------------------------------
 const onSignOut = async () => {
-  try {
-    const response = await signOut();
+  if (confirm('Do you want to sign out of MediTrack')) {
+    try {
+      const response = await signOut();
 
-    console.log('Signed out:', response);
-  } catch (error) {
-    console.log('Sign out error:', error);
-  } finally {
-    window.alert('Do you want to sign out of MediTrack');
-    window.location.href = "../onboarding.html";
+      console.log('Signed out:', response);
+    } catch (error) {
+      console.log('Sign out error:', error);
+    }
   }
 };
+
 
 
 //----------------------------------------
 //  Add event listeners
 //----------------------------------------
 buttonSignOut.addEventListener('click', onSignOut, false);
-
-
+document.addEventListener('DOMContentLoaded', populateInfo);
 
 function populateInfo() {
   firebase.auth().onAuthStateChanged(user => {
     //Check if user is signed in
     if (user) {
-      currentUser = db.collection("users").doc(user.uid)
+      currentUser = db.collection('users').doc(user.uid);
       currentUser.get().then(userDoc => {
-        var userName = userDoc.data().name;
-        var email = userDoc.data().email;
-        var profiles = userDoc.data().profiles;
+        const userName = userDoc.data().name;
+        const email = userDoc.data().email;
+        const profiles = userDoc.data().profiles;
 
+        console.log('!!!!', userDoc.data());
         // if the data fields are not empty , then enrich form with the data
         if (userName != null) {
-          document.getElementById("userName").value = userName;
+          inputName.value = userName;
         }
         if (email != null) {
-          document.getElementById("email").value = email;
+          inputEmail.value = email;
+        }
+        if (avatar) {
+          avatarContainer.src = avatar;
         }
 
-      console.log(userName, email)
-      })
+        updateProfileList(profiles);
+
+        console.log('[populateInfo] user, email:', userName, email);
+      });
     } else {
       // User is not signed in
-      console.log("No user is signed in")
+      console.log('No user is signed in');
+
+      window.location.href = 'sign-in-scr.html';
     }
   });
 }
 
-populateInfo();
-
 function editUserInfo() {
-  document.getElementById("userInfo").disabled = false;
-  // change buttons
-  document.getElementById("saveBtn").removeAttribute("hidden");
-  document.getElementById("editBtn").setAttribute("hidden", true);
+  // hide "edit" button by showing "save" button
+  enterEditMode();
 }
 
-function saveUserInfo() {
-  var userName = document.getElementById("userName").value;
-  var email = document.getElementById("email").value;
-  currentUser.update({
-    name: userName,
-    email: email
-  }).then(
-    () => {
-      console.log("user document successfully updated");
+async function saveUserInfo() {
+  // get current values
+  let currentEmail;
+  let currentName;
 
-      // firebase.auth().currentUser.updateEmail(email).then(() => {
-      //   console.log("email is updated in Firebase")
-      // }).catch((error) => {
-      //   console.log("error during email update")
-      // })
-    }
-  )
-  document.getElementById("saveBtn").setAttribute("hidden", true);
-  document.getElementById("editBtn").removeAttribute("hidden")
+  // await for current values
+  await currentUser
+    .get()
+    .then((doc) => {
+      currentEmail = doc.data().email;
+      currentName = doc.data().name;
+
+    })
+    .catch((error) => {
+      console.log('[saveUserInfo] error getting current email', error);
+    });
+
+  // get updated values from html nodes
+  const name = inputName.value;
+  const email = inputEmail.value;
+
+  // create array for multiple requests
+  const promises = [];
+
+  // update name if changed
+  if (currentName !== name) {
+    const updateUserCollectionNamePromise = currentUser.update({ name });
+
+    promises.push(updateUserCollectionNamePromise);
+  }
+
+  // update email if changed
+  if (currentEmail !== email) {
+    // update user collection email
+    const updateUserCollectionEmailPromise = currentUser.update({ email });
+
+    promises.push(updateUserCollectionEmailPromise);
+
+    // update auth data
+    const user = window.authClient.currentUser;
+    const updateEmailPromise = user.updateEmail(email);
+
+    promises.push(updateEmailPromise);
+  }
+
+  // perform requests
+  if (promises.length) {
+    Promise.all(promises)
+      .then((response) => {
+        // the response is Array<undefined>
+        console.log('[saveUserInfo] success:', response);
+      })
+      .catch((errors) => {
+        // Array<Error>
+        console.log('[saveUserInfo] error:', errors);
+
+        // reset current state
+        inputEmail.value = currentEmail;
+        inputName.value = currentName;
+      });
+  } else {
+    console.log('[saveUserInfo] nothing to update');
+  }
+
+  // hide "save" button by showing "edit" button
+  exitEditMode();
 }
-
 
 
 function addProfile() {
