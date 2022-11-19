be4b3dc51a7162dbc6f6ea11be43efc8c20655f7
Olga Zimina <olga.vic.zimina@gmail.com>
2022-11-18 22:26:21 -0800

    Remove unnecessary variable

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -22,4 +22,3 @@ const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const authClient = auth();
 window.authClient = authClient;
-var storage = firebase.storage();
