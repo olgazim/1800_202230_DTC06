3009164ebc3415641de690485ab4672cda0405c0
nataliecly <nyu17@my.bcit.ca>
2022-11-24 09:44:26 -0800

    Add "set another notification" button function

--- scripts/set-notification.js
+++ scripts/set-notification.js
@@ -1,46 +1,46 @@
 let currentUserId;
 
-const inputDate = document.getElementById('StartDate');
-const inputTime = document.getElementById('StartTime');
-const inputOption = document.getElementById('medication-options');
+const inputDate = document.getElementById("StartDate");
+const inputTime = document.getElementById("StartTime");
+const inputOption = document.getElementById("medication-options");
 
-inputDate.addEventListener('change', onDateChange);
-inputTime.addEventListener('change', onTimeChange);
+inputDate.addEventListener("change", onDateChange);
+inputTime.addEventListener("change", onTimeChange);
 
 function onDateChange(event) {
-  console.log('DATE:', event.target.value, inputOption.value);
+  console.log("DATE:", event.target.value, inputOption.value);
 }
 
 function onTimeChange(event) {
-  console.log('TIME:', event.target.value);
+  console.log("TIME:", event.target.value);
 }
 
 function hideCheckbox() {
-  $('#checkboxDays').hide();
+  $("#checkboxDays").hide();
 }
 
 function showCheckbox() {
-  $('#checkboxDays').show();
+  $("#checkboxDays").show();
 }
 
 function setup() {
-  var checkbox = document.getElementById('checkboxDays');
-  checkbox.style.display = 'none';
+  var checkbox = document.getElementById("checkboxDays");
+  checkbox.style.display = "none";
 
-  $('#form-check-input-selectDays').on('click', showCheckbox);
-  $('#form-check-input-everyDay').on('click', hideCheckbox);
+  $("#form-check-input-selectDays").on("click", showCheckbox);
+  $("#form-check-input-everyDay").on("click", hideCheckbox);
 }
 
 function getLoggedUser() {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       console.log(
-        `user logged in with email [${user.email}], id [${user.uid}]`,
+        `user logged in with email [${user.email}], id [${user.uid}]`
       );
       currentUserId = user.uid;
       fillMedicationList(currentUserId);
     } else {
-      alert('no logged in user');
+      alert("no logged in user");
     }
   });
 }
@@ -50,9 +50,10 @@ function setUrlParam(param, value) {
 
   searchParams.set(param, value);
 
-  const newRelativePathQuery = window.location.pathname + '?' + searchParams.toString();
+  const newRelativePathQuery =
+    window.location.pathname + "?" + searchParams.toString();
 
-  history.pushState(null, '', newRelativePathQuery);
+  history.pushState(null, "", newRelativePathQuery);
 }
 
 function getUrlParam(param) {
@@ -62,26 +63,28 @@ function getUrlParam(param) {
 }
 
 function fillMedicationList(userId) {
-  db.collection('users')
+  db.collection("users")
     .doc(userId)
-    .collection('medications')
+    .collection("medications")
     .get()
     .then((allMeds) => {
-      const medicationId = getUrlParam('medicationId');
-      console.log('MedicationId', medicationId);
-      $('#medication-options')
+      const medicationId = getUrlParam("medicationId");
+      console.log("MedicationId", medicationId);
+      $("#medication-options")
         // .append(`
         //   <option disabled selected value> -- select medicate -- </option>
         // `)
         .change((event) => {
-          setUrlParam('medicationId', event.target.value);
+          setUrlParam("medicationId", event.target.value);
         });
 
       allMeds.forEach((docRef) => {
         const doc = docRef.data();
 
-        $('#medication-options').append(`
-          <option ${medicationId === docRef.id ? 'selected' : ''} value="${docRef.id}">${doc.name}</option>
+        $("#medication-options").append(`
+          <option ${medicationId === docRef.id ? "selected" : ""} value="${
+          docRef.id
+        }">${doc.name}</option>
         `);
       });
     });
@@ -89,43 +92,57 @@ function fillMedicationList(userId) {
 
 function validateNotificationParams(params) {
   const { dateTime, dosage } = params;
-
 }
 
 function addNotification() {
   console.log("add notification");
   const date = inputDate.value;
   const time = inputTime.value;
-  const dateTime = firebase.firestore.Timestamp.fromDate(new Date(`${date}T${time}`));
+  const dateTime = firebase.firestore.Timestamp.fromDate(
+    new Date(`${date}T${time}`)
+  );
   const medicationId = inputOption.value;
-  const notificationId = getUrlParam('notificationId');
+  const notificationId = getUrlParam("notificationId");
 
-//   if (notificationId) {
-//     // updateFlow
-//   } else {
-    // add flow
-    db.collection("users")
-      .doc(currentUserId)
-      .collection("medications")
-      .doc(medicationId)
-      .collection("notifications")
-      .add({
-        dateTime,
-        dosage: '123mg',
-        profile: '',
-      })
-      .then((response) => {
-          console.log('Added:', response);
-          window.alert("Notification is set!");
+  //   if (notificationId) {
+  //     // updateFlow
+  //   } else {
+  // add flow
+  db.collection("users")
+    .doc(currentUserId)
+    .collection("medications")
+    .doc(medicationId)
+    .collection("notifications")
+    .add({
+      dateTime,
+      dosage: "123mg",
+      profile: "",
+    })
+    .then((response) => {
+      console.log("Added:", response);
+      window.alert("Notification is set!");
       window.location.assign("notification-scr.html");
-      })
-      .catch((error) => {
-        console.error("Error: ", error);
-      });
-//   }
-
+    })
+    .catch((error) => {
+      console.error("Error: ", error);
+    });
+  //   }
 }
 
 getLoggedUser();
+
+$("#setAnotherButton").on("click", function () {
+  $("#setAnotherReminder").append(
+    `
+    <div id="pillsListTitle">
+    <div>
+      <label for="medication-options">Select medication:</label>
+      <select id="medication-options">
+        <option disabled selected value> -- select medicate -- </option>
+      </select>
+    </div>
+    `
+  );
+});
 
 $(document).ready(setup);
